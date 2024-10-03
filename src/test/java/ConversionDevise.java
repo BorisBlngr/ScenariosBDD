ce1699ea951198c2eddd27ad3690a80e9f8e7806
Bobo <boris.boulanger@vidal.fr>
2024-10-03 16:30:44 +0200

    refacto

--- src/test/java/ConversionDevise.java
+++ src/test/java/ConversionDevise.java
@@ -4,20 +4,17 @@ import io.cucumber.java.en.When;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConversionDevise {
-   private Double montantDeviseCible;
-   private String deviseCible;
    private Montant notreMontant;
 
    @When("Je convertis {double} {string} en {string}")
    public void jeConvertisEn(Double montant, String deviseSource, String deviseCible) {
       if (montant < 0) return;
-      montantDeviseCible = montant;
-      this.deviseCible = deviseCible;
+      notreMontant = new Montant(montant, deviseCible);
    }
 
    @Then("Je dois avoir {double} {string}")
    public void jeDoisAvoir(Double montant, String deviseCible) {
-      assertEquals(new Montant(montant, deviseCible), new Montant(montantDeviseCible, this.deviseCible));
+      assertEquals(new Montant(montant, deviseCible), this.notreMontant);
    }
 
    @Then("Je ne peux pas convertir")
